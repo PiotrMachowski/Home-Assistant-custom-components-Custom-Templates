93751b7bdb19f5dd251a920d12c346249bfdf1bb
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2023-05-03 02:36:56 +0200

    Add ct_state_attr_translated function

--- custom_components/custom_templates/__init__.py
+++ custom_components/custom_templates/__init__.py
@@ -11,8 +11,8 @@ from homeassistant.helpers.translation import _TranslationCache, TRANSLATION_FLA
 from homeassistant.loader import bind_hass
 
 from .const import (DOMAIN, CUSTOM_TEMPLATES_SCHEMA, CONF_PRELOAD_TRANSLATIONS, CONST_EVAL_FUNCTION_NAME,
-                    CONST_STATE_TRANSLATED_FUNCTION_NAME, CONST_TRANSLATED_FUNCTION_NAME,
-                    CONST_ALL_TRANSLATIONS_FUNCTION_NAME)
+                    CONST_STATE_TRANSLATED_FUNCTION_NAME, CONST_STATE_ATTR_TRANSLATED_FUNCTION_NAME,
+                    CONST_TRANSLATED_FUNCTION_NAME, CONST_ALL_TRANSLATIONS_FUNCTION_NAME)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -77,6 +77,49 @@ class StateTranslated(TranslatableTemplate):
 
     def __repr__(self):
         return "<template StateTranslated>"
+
+
+class StateAttrTranslated(TranslatableTemplate):
+
+    def __init__(self, hass: HomeAssistant, available_languages):
+        super().__init__(hass, available_languages)
+
+    def __call__(self, entity_id: str, attribute: str, language: str):
+        self.validate_language(language)
+        state = None
+        if "." in entity_id:
+            state = _get_state_if_valid(self._hass, entity_id)
+
+        else:
+            if entity_id in _RESERVED_NAMES:
+                return None
+
+            if not valid_entity_id(f"{entity_id}.entity"):
+                raise TemplateError(f"Invalid domain name '{entity_id}'")  # type: ignore[arg-type]
+
+        if state is None:
+            return STATE_UNKNOWN
+        attribute_value = None
+        if attribute in state.attributes:
+            attribute_value = state.attributes.get(attribute)
+        entry = async_get(self._hass).async_get(entity_id)
+        translations = get_cached_translations(self._hass, language, "entity_component")
+        key = f"component.{state.domain}.entity_component._.state_attributes.{attribute}.state.{attribute_value}"
+        if len(translations) > 0 and key in translations:
+            return str(translations[key])
+        if (entry is not None and
+                entry.unique_id is not None and
+                hasattr(entry, "translation_key") and
+                entry.translation_key is not None):
+            key = f"component.{entry.platform}.entity.{state.domain}.{entry.translation_key}.state_attributes.{attribute}.state.{attribute_value}"
+            translations = get_cached_translations(self._hass, language, "entity")
+        if len(translations) > 0 and key in translations:
+            return str(translations[key])
+        _LOGGER.warning(f"No translation found for entity: {entity_id} and attribute: {attribute}")
+        return attribute_value
+
+    def __repr__(self):
+        return "<template StateAttrTranslated>"
 
 
 class Translated(TranslatableTemplate):
@@ -200,15 +243,17 @@ def setup(hass, config):
     _TranslationCache.get_cached = get_cached
 
     def is_safe_callable(self, obj):
-        return (isinstance(obj, (StateTranslated, EvalTemplate, Translated, AllTranslations))
+        return (isinstance(obj, (StateTranslated, StateAttrTranslated, EvalTemplate, Translated, AllTranslations))
                 or self.is_safe_callable_old(obj))
 
     def patch_environment(env: TemplateEnvironment):
         env.globals[CONST_STATE_TRANSLATED_FUNCTION_NAME] = state_translated_template
+        env.globals[CONST_STATE_ATTR_TRANSLATED_FUNCTION_NAME] = state_attr_translated_template
         env.globals[CONST_TRANSLATED_FUNCTION_NAME] = translated_template
         env.globals[CONST_ALL_TRANSLATIONS_FUNCTION_NAME] = all_translations_template
         env.globals[CONST_EVAL_FUNCTION_NAME] = eval_template
         env.filters[CONST_STATE_TRANSLATED_FUNCTION_NAME] = state_translated_template
+        env.filters[CONST_STATE_ATTR_TRANSLATED_FUNCTION_NAME] = state_attr_translated_template
         env.filters[CONST_TRANSLATED_FUNCTION_NAME] = translated_template
         env.filters[CONST_EVAL_FUNCTION_NAME] = eval_template
 
@@ -216,6 +261,7 @@ def setup(hass, config):
     TemplateEnvironment.is_safe_callable = is_safe_callable
 
     state_translated_template = StateTranslated(hass, languages)
+    state_attr_translated_template = StateAttrTranslated(hass, languages)
     translated_template = Translated(hass, languages)
     all_translations_template = AllTranslations(hass, languages)
     eval_template = EvalTemplate(hass)
