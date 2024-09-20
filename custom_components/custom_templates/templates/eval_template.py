ae7017b581fc037251eeb46872b7f5b14e983569
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2024-09-20 04:56:56 +0200

    Add passing context

--- custom_components/custom_templates/templates/eval_template.py
+++ custom_components/custom_templates/templates/eval_template.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.template import Template
+from jinja2.runtime import Context
 
 
 class EvalTemplate:
@@ -9,10 +10,17 @@ class EvalTemplate:
     def __init__(self, hass: HomeAssistant) -> None:
         self._hass = hass
 
-    def __call__(self, passed_context: dict, content: str, variables: dict | None = None, parse_result: bool = True, pass_context: bool = True) -> Any:
+    def __call__(
+            self,
+            passed_context: Context,
+            content: str,
+            variables: dict | None = None,
+            parse_result: bool = True,
+            pass_context: bool = True
+    ) -> Any:
         context = {}
         if pass_context:
-            context = context | pass_context
+            context = context | passed_context.get_all()
         if variables is not None:
             context = context | variables
         tpl = Template(content, self._hass)
